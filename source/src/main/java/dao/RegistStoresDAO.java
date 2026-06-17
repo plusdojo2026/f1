f8e9fc8787170af4657bbac2501dd6d9a4886f97
plusdojo2026-miyazakimana <miyazaki-mana-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 08:55:02 +0900

    RegistStoresDAO

--- source/src/main/java/dao/RegistStoresDAO.java
+++ source/src/main/java/dao/RegistStoresDAO.java
@@ -1,7 +1,53 @@
 package dao;
-public class RegistStoresDAO {
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import dto.RegistStoresDTO;
+
+
+
+public class RegistStoresDAO {
+	// regist_storesに登録する
+	public boolean insert(RegistStoresDTO dto) {
+		Connection conn = null;
+		boolean registResult = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f1?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+
+			// SQL文を準備する
+			String sql = "INSERT INTO regist_stores (user_id,store_id) VALUES(?,?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setInt(1, dto.getUser_id());
+			pStmt.setInt(2, dto.getStore_id());
+
+			if(pStmt.executeUpdate() ==1) {
+				registResult = true;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}	
+		return registResult;
 	}
 }
