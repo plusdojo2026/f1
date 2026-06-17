7d74adfbe289e08ccc3a66fbdc8404b3384c7ed6
plusdojo2026-oshima <ooshima-hiroto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 17:38:27 +0900

    DTOデータ型の変更

--- source/src/main/java/dto/FeaturedItemsDTO.java
+++ source/src/main/java/dto/FeaturedItemsDTO.java
@@ -4,22 +4,22 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 public class FeaturedItemsDTO implements Serializable {
-	private int featured_item_id;	//目玉商品ID
-	private int store_id;			//店舗ID
-	private int price;				//目玉商品価格
-	private String featured_name;	//目玉商品名
-	private LocalDate start_date;	//目玉商品開始日
-	private LocalDate end_date;		//目玉商品終了日
-	private String ap_name;			//代替商品名
+	private int featured_item_id;		//目玉商品ID
+	private int store_id;				//店舗ID
+	private int price;					//目玉商品価格
+	private String featured_item_name;	//目玉商品名
+	private Object start_date;			//目玉商品開始日
+	private Object end_date;			//目玉商品終了日
+	private String ap_name;				//代替商品名
 	
 	//コンストラクタ
-	public FeaturedItemsDTO(int featured_item_id, int store_id, int price, String featured_name, LocalDate start_date,
-			LocalDate end_date, String ap_name) {
+	public FeaturedItemsDTO(int featured_item_id, int store_id, int price, String featured_name, Object start_date,
+			Object end_date, String ap_name) {
 		super();
 		this.featured_item_id = featured_item_id;
 		this.store_id = store_id;
 		this.price = price;
-		this.featured_name = featured_name;
+		this.featured_item_name = featured_name;
 		this.start_date = start_date;
 		this.end_date = end_date;
 		this.ap_name = ap_name;
@@ -31,7 +31,7 @@ public class FeaturedItemsDTO implements Serializable {
 		this.featured_item_id = 0;
 		this.store_id = 0;
 		this.price = 0;
-		this.featured_name = "";
+		this.featured_item_name = "";
 		this.start_date = null;
 		this.end_date = null;
 		this.ap_name = "";
@@ -61,15 +61,15 @@ public class FeaturedItemsDTO implements Serializable {
 		this.price = price;
 	}
 
-	public String getFeatured_name() {
-		return featured_name;
+	public String getFeatured_item_name() {
+		return featured_item_name;
 	}
 
-	public void setFeatured_name(String featured_name) {
-		this.featured_name = featured_name;
+	public void setFeatured_item_name(String featured_name) {
+		this.featured_item_name = featured_name;
 	}
 
-	public LocalDate getStart_date() {
+	public Object getStart_date() {
 		return start_date;
 	}
 
@@ -77,7 +77,7 @@ public class FeaturedItemsDTO implements Serializable {
 		this.start_date = start_date;
 	}
 
-	public LocalDate getEnd_date() {
+	public Object getEnd_date() {
 		return end_date;
 	}
 
