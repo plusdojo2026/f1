46e1808d822cc313ebd22a3e1bcdc0ae145ea995
plusdojo2026-miyazakimana <miyazaki-mana-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 10:59:40 +0900

    RegistStoresDAO最新

--- source/src/main/java/dao/RegistStoresDAO.java
+++ source/src/main/java/dao/RegistStoresDAO.java
@@ -1,4 +1,6 @@
 package dao;
+//店舗の登録・削除を行うDAO
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -22,14 +24,17 @@ public class RegistStoresDAO {
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f1?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+					"f1", "xVyQPJuerzK8LB4G");
 
 			// SQL文を準備する
 			String sql = "INSERT INTO regist_stores (user_id,store_id) VALUES(?,?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
 			pStmt.setInt(1, dto.getUser_id());
 			pStmt.setInt(2, dto.getStore_id());
 
+			// SQL文を実行する
 			if(pStmt.executeUpdate() ==1) {
 				registResult = true;
 			}
@@ -48,6 +53,52 @@ public class RegistStoresDAO {
 				}
 			}
 		}	
+		return registResult;
+	}
+
+	//regist_storesから削除する
+	public boolean delete(RegistStoresDTO dto) {
+		Connection conn = null;
+		boolean registResult = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f1?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"f1", "xVyQPJuerzK8LB4G");
+
+			// SQL文を準備する
+			String sql = "DELETE FROM regist_stores WHERE user_id=? AND store_id=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, dto.getUser_id());
+			pStmt.setInt(2, dto.getStore_id());
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				registResult = true;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
 		return registResult;
 	}
 }
