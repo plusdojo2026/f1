f4994864fb025b917258d888dba271d6001231a8
plusdojo2026-hara <hara-masato-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 15:32:26 +0900

    ログイン関係

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -1,5 +1,49 @@
 package servlet;
 
-public class LoginServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.IdPwDAO;
+import dto.UsersDTO;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String address = request.getParameter("address");
+		String password = request.getParameter("password");
+
+		// ログイン処理を行う
+		IdPwDAO iDao = new IdPwDAO();
+		if (iDao.isLoginOK(new UsersDTO(0,address,password,"","",""))) { // ログイン成功
+			// addressとpasswordをリクエストスコープに格納
+			request.setAttribute("address", address);
+			request.setAttribute("password", password);
+
+			// ログインページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
 }
