5e39d30f1ab36c1682e265051c1f96cede0fe3d9
plusdojo2026-miyazakimana <miyazaki-mana-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-23 13:32:52 +0900

    RegistStoresDAO更新

--- source/src/main/java/dao/RegistStoresDAO.java
+++ source/src/main/java/dao/RegistStoresDAO.java
@@ -26,7 +26,7 @@ public class RegistStoresDAO {
 					"f1", "xVyQPJuerzK8LB4G");
 
 			// SQL文を準備する
-			String sql = "SELECT user_id, store_id FROM regist_stores"
+			String sql = "SELECT user_id, phone_number FROM regist_stores"
 					+ "WHERE user_id LIKE ? AND phone_number LIKE ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
