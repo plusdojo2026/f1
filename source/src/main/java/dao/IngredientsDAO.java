af58c0a3fbde477264de118d665db06e5944d529
plusdojo2026-miyazakimana <miyazaki-mana-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 17:59:50 +0900

    更新

--- source/src/main/java/dao/IngredientsDAO.java
+++ source/src/main/java/dao/IngredientsDAO.java
@@ -21,7 +21,7 @@ public class IngredientsDAO {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/miya?"
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f1?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true&allowPublicKeyRetrieval=true", "f1",
 					"xVyQPJuerzK8LB4G");
 
