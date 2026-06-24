a4b9d327337d032da07e5d8106c97749caa3a0ab
plusdojo2026-osuka <osuka-shogo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 14:37:34 +0900

    Update UsersDAO.java

--- source/src/main/java/dao/UsersDAO.java
+++ source/src/main/java/dao/UsersDAO.java
@@ -23,10 +23,10 @@ public class UsersDAO {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/goodbuy?"
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f1?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true"
 					+ "&allowPublicKeyRetrieval=true",
-					"root", "password");
+					"f1", "xVyQPJuerzK8LB4G");
 
 			// SQL文を準備する
 			String sql = "SELECT user_id, address, password, prefecture_id, memo FROM users "
@@ -98,9 +98,9 @@ public class UsersDAO {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/goodbuy?"
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f1?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+					"f1", "xVyQPJuerzK8LB4G");
 			
 			//現在の最大user_idを取得
 			String maxSql = "SELECT COALESCE(MAX(user_id), 0) AS max_id FROM users";
@@ -178,9 +178,9 @@ public class UsersDAO {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/goodbuy?"
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f1?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+					"f1", "xVyQPJuerzK8LB4G");
 
 			// SQL文を準備する
 			String sql = "UPDATE users SET address=?, password=?, prefecture_id=?, memo=? WHERE user_id = ?";
