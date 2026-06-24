bfac13cadc48c9ea0605b140a298049c9fe11748
plusdojo2026-kuwahara <kuwahara-yugo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 12:00:22 +0900

    SQL修正　レシピ追加

---
+++ source/sql/api.sql
@@ -0,0 +1,10 @@
+ create table API(
+  api_id INT PRIMARY KEY,
+  api_key VARCHAR(100));
+  
+  insert into API(
+  api_id,
+  api_key)
+  
+  VALUES(1,'AQ.Ab8RN6L-WofRjwi51eDdqvvyPZ_tTvUkILj1umR4yX3bvFyINA');
+  
