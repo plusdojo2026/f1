65ddb225d958c251645b73ab60958539998642c0
plusdojo2026-kuwahara <kuwahara-yugo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 10:21:43 +0900

    SQL修正

--- source/sql/f1.sql
+++ source/sql/f1.sql
@@ -849,13 +849,6 @@ VALUES
  create table API(
   api_id INT PRIMARY KEY,
   api_key VARCHAR(100));
-  
-  insert into API(
-  api_id,
-  api_key)
-  
- VALUES(1,'AQ.Ab8RN6L-WofRjwi51eDdqvvyPZ_tTvUkILj1umR4yX3bvFyINA');
-
 
 DELETE ap
 FROM average_prices ap
