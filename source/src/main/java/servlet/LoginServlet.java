8bc716b7edd907b6674d845a9888fad3ae6e79a1
plusdojo2026-osuka <osuka-shogo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-25 15:52:12 +0900

    Update LoginServlet.java

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -15,7 +15,7 @@ import dto.LoginUserDTO;
 import dto.ResultDTO;
 import dto.UsersDTO;
 
-@WebServlet("")
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
